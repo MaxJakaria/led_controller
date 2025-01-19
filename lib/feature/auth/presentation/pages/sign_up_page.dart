5ca876e126d027fb4195dce6d3723c9fad2886e2
MaxJakaria <maxjakaria825@gmail.com>
2025-01-19 16:33:58 +0600

    sign_up_page, theme and required widgets

---
+++ lib/feature/auth/presentation/pages/sign_up_page.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:led_controller/core/theme/app_pallete.dart';
+import 'package:led_controller/feature/auth/presentation/widgets/auth_field.dart';
+import 'package:led_controller/feature/auth/presentation/widgets/auth_gradient_button.dart';
+
+class SignupPage extends StatefulWidget {
+  const SignupPage({super.key});
+
+  @override
+  State<SignupPage> createState() => _SignupPageState();
+}
+
+class _SignupPageState extends State<SignupPage> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final nameController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    nameController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Form(
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+
+            children: [
+              const Text(
+                'Sign Up.',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 50),
+              ),
+              AuthField(hintText: 'Name', controller: nameController),
+              const SizedBox(height: 15),
+              AuthField(hintText: 'Email', controller: emailController),
+              const SizedBox(height: 15),
+              AuthField(
+                hintText: 'Password',
+                controller: passwordController,
+                isObscureText: true,
+              ),
+              const SizedBox(height: 60),
+              const AuthGradientButton(buttonName: 'Sign Up'),
+              const SizedBox(height: 20),
+              RichText(
+                text: TextSpan(
+                  text: 'Don\'t have an account? ',
+                  style: Theme.of(context).textTheme.titleMedium,
+                  children: [
+                    TextSpan(
+                      text: 'Sign In',
+                      style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                        color: AppPallete.gradient2,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/feature/auth/presentation/widgets/auth_gradient_button.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:led_controller/core/theme/app_pallete.dart';
+
+class AuthGradientButton extends StatelessWidget {
+  final String buttonName;
+  const AuthGradientButton({super.key, required this.buttonName});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: const LinearGradient(
+          colors: [AppPallete.gradient1, AppPallete.gradient2],
+          begin: Alignment.bottomLeft,
+          end: Alignment.topRight,
+        ),
+        borderRadius: BorderRadius.circular(7),
+      ),
+      child: ElevatedButton(
+        onPressed: () {},
+        style: ElevatedButton.styleFrom(
+          backgroundColor: AppPallete.transparentColor,
+          shadowColor: AppPallete.transparentColor,
+          fixedSize: const Size(395, 55),
+        ),
+        child: Text(
+          buttonName,
+          style: const TextStyle(fontSize: 17, fontWeight: FontWeight.w600),
+        ),
+      ),
+    );
+  }
+}
